a96a8ddddadbad7c90be323047e4244fccc1694b
jasecode <jzhang@mobify.com>
2020-01-07 16:49:16 -0800

    practice making simple assertion library

--- assertion-library.js
+++ assertion-library.js
@@ -1,15 +1,30 @@
-const {sum, subtract} = require('../math')
+const {sum, subtract} = require('./math')
 
 let result, expected
 
 result = sum(3, 7)
 expected = 10
-if (result !== expected) {
-    throw new Error(`${result} is not equal to ${expected}`)
-}
+expect(result).toBe(expected)
 
 result = subtract(7, 3)
 expected = 4
-if (result !== expected) {
-    throw new Error(`${result} is not equal to ${expected}`)
+expect(result).toBe(expected)
+
+result = subtract(0, 5)
+expected = 0
+expect(result).toBeGreaterThan(expected)
+
+function expect(actual){
+    return {
+        toBe(expected) {
+            if(actual !== expected) {
+                throw new Error(`${result} is not equal to ${expected}`)
+            }
+        },
+        toBeGreaterThan(expected) {
+            if(actual >= expected) {
+                throw new Error(`${result} is greater than ${expected}`)
+            }
+        }
+    }
 }
